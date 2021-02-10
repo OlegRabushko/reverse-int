ded2caa0d489178a2db7d947da4161bc66cab181
Oleg <oleg.rabyshko@mail.ru>
2021-02-10 14:48:47 +0300

    feat: add passed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let a = [];
+    a.push(n);
+    let b = String(a)
+        .split("")
+        .reverse()
+        .join("")
+        .replace(/[^0-9]/g, "");
+    return Number(b);
+};
